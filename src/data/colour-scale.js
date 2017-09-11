ec00cd75f432003a72c067c88e5015521e5cdbd6
Nathan Hoad <nathan@nathanhoad.net>
2017-09-11 13:03:34 +1000

    Changed the colours of the scale

--- src/data/colour-scale.js
+++ src/data/colour-scale.js
@@ -1,30 +1,19 @@
 const Scale = require('d3-scale');
 
 const colours = [
-    '#025e89',
-    '#2d6b93',
-    '#46789d',
-    '#5c85a7',
-    '#6f92b1',
-    '#83a0bb',
-    '#96afc6',
-    '#a9bdd0',
-    '#bbcbda',
-    '#cfdae4',
-    '#e2e8ef',
-    '#f5f8fa',
-    '#fdf6f7',
-    '#f9e2e6',
-    '#f4d0d5',
-    '#eebdc5',
-    '#e8aab5',
-    '#e297a5',
-    '#db8495',
-    '#d37187',
-    '#cb5e78',
-    '#c34869',
-    '#b92f5b',
-    '#b0064d'
+    '#e86b2b',
+    '#f2844f',
+    '#f99d70',
+    '#feb593',
+    '#ffcfb7',
+    '#ffe7dc',
+    '#ffffff',
+    '#e1eff7',
+    '#c2e0ef',
+    '#a2d0e7',
+    '#7fc1df',
+    '#56b2d7',
+    '#00a3cf'
 ];
 
 module.exports = () => {
